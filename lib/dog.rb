bb7f513222b415788250af8a1d564bde91737e67
Steve <stephenkago10@gmail.com>
2022-10-20 21:56:24 +0300

    Initial commit

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
-
+    #setter
+    def name=(dog_name)
+      @this_dogs_name = dog_name
+    end
+  
+    #getter
+    def name
+      @this_dogs_name
+    end
 end
+
+#create an instance and assign a name
+lassie = Dog.new
+lassie.name = "Lassie"
+
+#call to output
+puts lassie.name
+
